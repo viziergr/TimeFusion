d445a7b7fcbf7ac2838a5465c1e97f69d8c0f10b
viziergr <gregoire.vizier@gmail.com>
2024-01-22 13:08:14 +0100

    Update event.sql and LocalStorage.json

--- sql/event.sql
+++ sql/event.sql
@@ -3,9 +3,9 @@ CREATE TABLE `event` (
   `title` varchar(45) NOT NULL,
   `start_time` datetime NOT NULL,
   `end_time` datetime NOT NULL,
-  `location` varchar(45) DEFAULT NULL,
+  `location` varchar(45) DEFAULT 'eseo',
   `description` varchar(255) DEFAULT NULL,
-  `is_private` tinyint(1) DEFAULT NULL,
+  `is_private` tinyint(1) DEFAULT 0,
   `creator_id` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
